72520ff0fafdd6d41215573825dc4d4d3a68ddcd
BobTheBob <32057864+BobTheBob9@users.noreply.github.com>
2021-12-24 23:03:44 +0000

    Revert "sort server list by playercount"
    This reverts commit bbd424e5ce25af3fc22dc171331abdb17e82a5c7.

--- client/serverlist.js
+++ client/serverlist.js
@@ -39,7 +39,7 @@ module.exports = ( fastify, opts, done ) => {
 		for ( let server of expiredServers )
 			RemoveGameServer( server )
 		
-		return displayServerArray.sort( ( a, b ) => b.playerCount - a.playerCount )
+		return displayServerArray
 	})
 	
 	done()

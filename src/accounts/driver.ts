0a9d19b3667c17e04481c611289d9f5f63b3fb38
Jack Baron <jackmbaron@gmail.com>
2021-12-31 12:11:35 +0000

    add query builder property to driver

--- src/accounts/driver.ts
+++ src/accounts/driver.ts
@@ -1,5 +1,9 @@
+import { type Knex } from 'knex'
+
 export type Row = Record<string, unknown>
 export interface SQLDriver {
+  readonly builder: Knex
+
   query(query: string, parameters: unknown[]): Promise<Row[]>
   querySingle(query: string, parameters: unknown[]): Promise<Row | undefined>
   execute(query: string, parameters: unknown[]): Promise<void>

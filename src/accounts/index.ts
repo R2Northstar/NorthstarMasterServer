b30f5ac39d04fabb3ae594bfdb33acab61ece9ea
Jack Baron <jackmbaron@gmail.com>
2021-12-31 12:34:23 +0000

    use knex native driver

--- src/accounts/index.ts
+++ src/accounts/index.ts
@@ -1,1 +1,21 @@
+import knex from 'knex'
+
+// TODO: Implement selection
+export const db = knex({
+  client: 'sqlite',
+  useNullAsDefault: true,
+  connection: { filename: './playerdata.db' },
+})
+
+const hasAccountsTable = await db.schema.hasTable('accounts')
+if (!hasAccountsTable) {
+  await db.schema.createTable('accounts', table => {
+    table.text('id').notNullable().primary()
+    table.text('authToken')
+    table.integer('authTokenExpireTime')
+    table.text('currentServerID')
+    table.binary('persistentDataBaseline').notNullable()
+  })
+}
+
 export { createAccount, getById, getOrCreate } from './account.js'

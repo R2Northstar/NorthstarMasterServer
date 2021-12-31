c0f9b6191fb412283159e5e6f8dd925428072829
Jack Baron <jackmbaron@gmail.com>
2021-12-31 15:37:58 +0000

    fix cors

--- src/routes/client/main_menu_promos.ts
+++ src/routes/client/main_menu_promos.ts
@@ -1,5 +1,4 @@
 import { type FastifyPluginAsync } from 'fastify'
-import cors from 'fastify-cors'
 import fastifyStatic from 'fastify-static'
 import { PUBLIC_ASSETS_DIR } from '~constants.js'
 
@@ -7,7 +6,6 @@ import { PUBLIC_ASSETS_DIR } from '~constants.js'
 // returns main menu promo info
 
 const register: FastifyPluginAsync = async (fastify, _) => {
-  await fastify.register(cors, { preflight: false })
   await fastify.register(fastifyStatic, { root: PUBLIC_ASSETS_DIR })
 
   fastify.get('/client/mainmenupromos', async (_, response) => {

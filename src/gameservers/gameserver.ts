b73d8007a5ff143634ab8e4ef5a531093d5632ed
Jack Baron <jackmbaron@gmail.com>
2021-12-31 18:31:27 +0000

    add (de)serializer for GameServer

--- src/gameservers/gameserver.ts
+++ src/gameservers/gameserver.ts
@@ -1,3 +1,5 @@
+import { type Decoder, type Encoder } from '@msgpack/msgpack'
+import { Buffer } from 'node:buffer'
 import { randomBytes } from 'node:crypto'
 import { type OnlyProperties } from '~utils.js'
 
@@ -18,6 +20,8 @@ export interface ModInfo {
 }
 
 export interface GameServerOptions {
+  id?: string
+  serverAuthToken?: string
   name: string
   description: string
   playerCount: number
@@ -29,9 +33,44 @@ export interface GameServerOptions {
   authPort: number
   password?: string
   modInfo?: ModInfo
+  lastHeartbeat?: Date
 }
 
 export class GameServer {
+  public static decode(buffer: Buffer, decoder: Decoder): GameServer {
+    const raw = decoder.decode(buffer)
+    if (typeof raw !== 'object' || raw === null) {
+      throw new Error('invalid serialized GameServer')
+    }
+
+    const checkValid: (r: Record<string, unknown>) => boolean = r => {
+      // Check fields exist
+      if ('id' in r === false) return false
+      if ('authToken' in r === false) return false
+      if ('name' in r === false) return false
+      if ('description' in r === false) return false
+      if ('playerCount' in r === false) return false
+      if ('maxPlayers' in r === false) return false
+      if ('map' in r === false) return false
+      if ('playlist' in r === false) return false
+      if ('ip' in r === false) return false
+      if ('port' in r === false) return false
+      if ('authPort' in r === false) return false
+      if ('password' in r === false) return false
+      if ('modInfo' in r === false) return false
+      if ('lastHeartbeat' in r === false) return false
+
+      return true
+    }
+
+    if (checkValid(raw as Record<string, unknown>) === false) {
+      throw new Error('invalid serialized GameServer')
+    }
+
+    const options = raw as GameServerOptions
+    return new GameServer(options)
+  }
+
   // #region Fields
   public readonly id: string
   public readonly serverAuthToken: string
@@ -48,13 +87,12 @@ export class GameServer {
   public readonly hasPassword: boolean
   public modInfo: ModInfo
   public lastHeartbeat: number
-
   // #endregion
 
   // #region Constructor
   constructor(options: GameServerOptions) {
-    this.id = randomBytes(16).toString('hex')
-    this.serverAuthToken = randomBytes(16).toString('hex')
+    this.id = options.id ?? randomBytes(16).toString('hex')
+    this.serverAuthToken = options.serverAuthToken ?? randomBytes(16).toString('hex')
     this.name = options.name
     this.description = options.description
     this.playerCount = options.playerCount
@@ -72,6 +110,17 @@ export class GameServer {
   // #endregion
 
   // #region Methods
+  public encode(encoder: Encoder): Buffer {
+    const encoded = encoder.encode(this)
+    const buffer = Buffer.from(
+      encoded.buffer,
+      encoded.byteOffset,
+      encoded.byteLength
+    )
+
+    return buffer
+  }
+
   public clean(): CleanGameServer {
     const clean: CleanGameServer = Object.freeze({
       id: this.id,

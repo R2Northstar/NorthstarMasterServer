65033065b496b061032101b1c8544cc235d977d3
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-01-12 10:16:41 +0000

    Start listening after sync code completes
    Prevents race conditions and incorrect data (i.e. requests could be served before the sync was done, so just trying to prevent potential data overwrites here)

--- datasyncing.js
+++ datasyncing.js
@@ -18,79 +18,86 @@ function shuffleArray(array) {
 }
 
 async function attemptSyncWithAny() {
-    accounts.BackupDatabase() // backup DB on startup in case of big oopsie
-
-    console.log("Attempting to sync with first available server")
-    setOwnState(1)
-    // Attempt to sync with any up-and-running masterserver
-    let hasSynced = false;
-    instances = shuffleArray(await getAllKnownInstances());
-    for(instance of instances) {
-        if(!instance.isSelf && !hasSynced) {
-            try {
-                let instanceState = await getInstanceState(instance);
-
-                if(instanceState == 2) {
-                    try {
-                        console.log("Attempting sync with instance "+instance.name)
-                        // Sync game servers
-                        let currentServers = GetGameServers();
-
-                        let servers = await serverSyncWithInstance(instance);
-
-                        for(let i = 0; i < Object.keys(servers).length; i++) {
-                            let id = Object.keys(servers)[i]
-                            if(currentServers[id]) {
-                                UpdateGameServer(currentServers[id], servers[id], false)
-                            } else {
-                                let { name, description, playerCount, maxPlayers, map, playlist, ip, port, authPort, password, modInfo, lastHeartbeat } = servers[id];
-                                let newServer = new GameServer( name, description, playerCount, maxPlayers, map, playlist, ip, port, authPort, password, modInfo, lastHeartbeat )
-                                newServer.id = id;
-                                newServer.lastHeartbeat = lastHeartbeat;
-                                AddGameServer(newServer, false)
+    return new Promise(async (resolve, reject) => {
+        accounts.BackupDatabase() // backup DB on startup in case of big oopsie
+    
+        console.log("Attempting to sync with first available server")
+        setOwnState(1)
+        // Attempt to sync with any up-and-running masterserver
+        let hasSynced = false;
+        instances = shuffleArray(await getAllKnownInstances());
+        for(instance of instances) {
+            if(!instance.isSelf && !hasSynced) {
+                try {
+                    let instanceState = await getInstanceState(instance);
+    
+                    if(instanceState == 2) {
+                        try {
+                            console.log("Attempting sync with instance "+instance.name)
+                            // Sync game servers
+                            let currentServers = GetGameServers();
+    
+                            let servers = await serverSyncWithInstance(instance);
+    
+                            for(let i = 0; i < Object.keys(servers).length; i++) {
+                                let id = Object.keys(servers)[i]
+                                if(currentServers[id]) {
+                                    UpdateGameServer(currentServers[id], servers[id], false)
+                                } else {
+                                    let { name, description, playerCount, maxPlayers, map, playlist, ip, port, authPort, password, modInfo, lastHeartbeat, lastModified } = servers[id];
+                                    let newServer = new GameServer( name, description, playerCount, maxPlayers, map, playlist, ip, port, authPort, password, modInfo, lastHeartbeat )
+                                    newServer.id = id;
+                                    newServer.lastHeartbeat = lastHeartbeat;
+                                    newServer.lastModified = lastModified;
+                                    AddGameServer(newServer, false)
+                                }
                             }
+                            console.log("Synced servers with instance "+instance.name)
+    
+                            // Sync accounts in DB
+                            let accountList = await accountSyncWithInstance(instance);
+    
+                            accountList.forEach(async accountJson => {
+                                let account = await accounts.AsyncGetPlayerByID( accountJson.id )
+                                if(accountJson.persistentDataBaseline) accountJson.persistentDataBaseline = Buffer.from(accountJson.persistentDataBaseline)
+                                if ( !account ) // create account for user
+                                {
+                                    await accounts.AsyncCreateAccountFromData( accountJson, accountJson.lastModified )
+                                    account = await accounts.AsyncGetPlayerByID( accountJson.id )
+                                } else {
+                                    if(accountJson.lastModified > account.lastModified) accounts.AsyncUpdatePlayer( account.id, accountJson.account, accountJson.lastModified )
+                                }
+                            });
+                            console.log("Synced accounts with instance "+instance.name)
+    
+                            console.log("Completed sync with instance "+instance.name)
+                            setOwnState(2)
+                            hasSynced = true;
+                        } catch(e) {
+                            console.log(e)
+                            console.log("Failed to complete sync with instance "+instance.name)
                         }
-                        console.log("Synced servers with instance "+instance.name)
-
-                        // Sync accounts in DB
-                        let accountList = await accountSyncWithInstance(instance);
-                        accountList.forEach(async accountJson => {
-                            let account = await accounts.AsyncGetPlayerByID( accountJson.id )
-                            if(accountJson.persistentDataBaseline) accountJson.persistentDataBaseline = Buffer.from(accountJson.persistentDataBaseline)
-                            if ( !account ) // create account for user
-                            {
-                                await accounts.AsyncCreateAccountFromData( accountJson )
-                                account = await accounts.AsyncGetPlayerByID( accountJson.id )
-                            } else {
-                                if(accountJson.lastModified > account.lastModified) accounts.AsyncUpdatePlayer( account.id, accountJson.account )
-                            }
-                        });
-                        console.log("Synced accounts with instance "+instance.name)
-
-                        console.log("Completed sync with instance "+instance.name)
-                        setOwnState(2)
-                        hasSynced = true;
-                    } catch(e) {
-                        console.log("Failed to complete sync with instance "+instance.name)
                     }
+                } catch(e) {
+                    // console.log(e)
                 }
-            } catch(e) {
-                // console.log(e)
             }
         }
-    }
+    
+        // Skip if none available
+        if(!hasSynced) {
+            console.log("Sync could not be completed")
+            setOwnState(2)
+        }
+    
+        // backup server every n minutes in case of oopsie
+        console.log(`Will attempt to backup DB every ${process.env.DB_BACKUP_MINUTES || 30} minutes`)
+        setInterval(() => {
+            accounts.BackupDatabase()
+        }, (process.env.DB_BACKUP_MINUTES || 30)*60000)
 
-    // Skip if none available
-    if(!hasSynced) {
-        console.log("Sync could not be completed")
-        setOwnState(2)
-    }
-
-    // backup server every n minutes in case of oopsie
-    console.log(`Will attempt to backup DB every ${process.env.DB_BACKUP_MINUTES || 30} minutes`)
-    setInterval(() => {
-        accounts.BackupDatabase()
-    }, (process.env.DB_BACKUP_MINUTES || 30)*60000)
+        resolve()
+    })
 }
 
 async function getInstanceState(instance) {
--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function start()
 	}
 }
 
-start()
-
 const syncing = require("./datasyncing.js")
-syncing.attemptSyncWithAny()
+syncing.attemptSyncWithAny().then(() => {
+	start()
+})

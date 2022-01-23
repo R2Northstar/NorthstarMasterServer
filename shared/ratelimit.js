0654d159e1f7066b10df20988ff4b4008bf619dc
Barnaby <barnab.why@gmail.com>
2022-01-23 20:35:45 +0000

    Ratelimit endpoints (#20)

---
+++ shared/ratelimit.js
@@ -0,0 +1,7 @@
+function getRatelimit(envVar) {
+    return { max: Number(process.env[envVar]) || (Number(process.env.REQ_PER_MINUTE__GLOBAL) || 9999) }
+}
+
+module.exports = {
+    getRatelimit
+}

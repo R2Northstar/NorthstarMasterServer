b9e9af0c357d855d32d2bb00a86467a35ff49bd6
Barnaby <barnab.why@gmail.com>
2022-02-20 23:02:36 +0000

    Add web landing page (#32)

--- client/redirect.js
+++ client/redirect.js
@@ -1,37 +1,41 @@
-const { getRatelimit } = require("../shared/ratelimit.js")
+const { getRatelimit } = require( "../shared/ratelimit.js" )
 
-module.exports = ( fastify, opts, done ) => {
+module.exports = ( fastify, opts, done ) =>
+{
 	// exported routes
 
-    // GET /
-    // redirect anyone going to northstar.tf in a browser to the github
-    fastify.get( '/',
-    {
-		config: { rateLimit: getRatelimit("REQ_PER_MINUTE__REDIRECT") }, // ratelimit
-    },
-    async ( request, reply ) => {
-        reply.redirect( "https://github.com/R2Northstar" )
-    })
+	// GET /
+	// redirect anyone going to northstar.tf/github in a browser to the github
+	fastify.get( "/github",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__REDIRECT" ) }, // ratelimit
+		},
+		async ( request, reply ) =>
+		{
+			reply.redirect( "https://github.com/R2Northstar" )
+		} )
 
-    // GET /discord
-    // redirect anyone going to northstar.tf/discord to the discord
-    fastify.get( '/discord',
-    {
-		config: { rateLimit: getRatelimit("REQ_PER_MINUTE__REDIRECT") }, // ratelimit
-    },
-    async ( request, reply ) => {
-        reply.redirect( "https://discord.gg/GYVRKC9pJh" )
-    })
+	// GET /discord
+	// redirect anyone going to northstar.tf/discord to the discord
+	fastify.get( "/discord",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__REDIRECT" ) }, // ratelimit
+		},
+		async ( request, reply ) =>
+		{
+			reply.redirect( "https://discord.gg/GYVRKC9pJh" )
+		} )
 
-    // GET /wiki
-    // redirect anyone going to northstar.tf/wiki to the wiki
-    fastify.get( '/wiki',
-    {
-		config: { rateLimit: getRatelimit("REQ_PER_MINUTE__REDIRECT") }, // ratelimit
-    },
-    async ( request, reply ) => {
-        reply.redirect( "https://r2northstar.gitbook.io/" )
-    })
+	// GET /wiki
+	// redirect anyone going to northstar.tf/wiki to the wiki
+	fastify.get( "/wiki",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__REDIRECT" ) }, // ratelimit
+		},
+		async ( request, reply ) =>
+		{
+			reply.redirect( "https://r2northstar.gitbook.io/" )
+		} )
 
-    done()
+	done()
 }

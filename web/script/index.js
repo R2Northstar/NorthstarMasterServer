9df50d11738fc2e8636dd9945c9e7e61e61685a1
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-02-20 21:19:09 +0000

    Add catch to the download url fetch, log cause why not

--- web/script/index.js
+++ web/script/index.js
@@ -1,8 +1,11 @@
 fetch('https://api.github.com/repos/R2Northstar/Northstar/releases/latest')
-  .then(response => response.json())
-  .then(data => {
-      document.querySelectorAll('.download').forEach(d => { d.href = data.assets[0].browser_download_url });
-  });
+    .then(response => response.json())
+    .then(data => {
+        document.querySelectorAll('.download').forEach(d => { d.href = data.assets[0].browser_download_url });
+        console.log("Successfully fetched latest release download URL. Updating download links.")
+    }).catch(() => {
+    console.log("Failed to fetch latest release download URL, ratelimit was likely reached. Download links will fallback to the GitHub latest release page.")
+    })
 
 document.addEventListener('scroll', () => {
     if(window.scrollY > 0) {

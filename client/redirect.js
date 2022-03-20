1d28ae284b81de7e8537711a06de89ec3391ca4b
Tristan Kennedy <tristan@taskinoz.com>
2022-03-20 12:55:14 +1100

    Added redirect for /thunderstore

--- client/redirect.js
+++ client/redirect.js
@@ -37,5 +37,16 @@ module.exports = ( fastify, opts, done ) =>
 			reply.redirect( "https://r2northstar.gitbook.io/" )
 		} )
 
+	// GET /wiki
+	// redirect anyone going to northstar.tf/wiki to the wiki
+	fastify.get( "/thunderstore",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__REDIRECT" ) }, // ratelimit
+		},
+		async ( request, reply ) =>
+		{
+			reply.redirect( "https://northstar.thunderstore.io/" )
+		} )
+
 	done()
 }

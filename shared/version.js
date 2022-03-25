881c193f7adfd003a5a6e8a528d602e50220e1ba
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-03-25 21:56:03 +0000

    fixes this stuff i hope

--- shared/version.js
+++ shared/version.js
@@ -1,11 +1,11 @@
-const versionRe = /(?:^R2Northstar\/)(\d+)\.(\d+)\.(\d+)/g
+const semver = require( "semver" )
 
 function minimumVersion( request )
 {
-	let v = versionRe.exec( request.headers["User-Agent"] )
-	if( !process.env.MINIMUM_LAUNCHER_VERSION || request.headers["User-Agent"].match ( /(\+dev)/ ) ) return true
-	let minV = process.env.MINIMUM_LAUNCHER_VERSION.split( "." )
-	if( v &&  v[1] >= minV[0] && v[2] >= minV[1] && v[3] >= minV[2] ) return true
+	if( !process.env.MINIMUM_LAUNCHER_VERSION ) return true
+	if( !request.headers["user-agent"] || !request.headers["user-agent"].startsWith( "R2Northstar/" ) ) return false
+	let v = request.headers["user-agent"].split( " " )[0].substring( 12 )
+	if( semver.valid( v ) && semver.satisfies( v, ">="+process.env.MINIMUM_LAUNCHER_VERSION ) || v.match( /\+dev/g ) ) return true
 	return false
 }
 

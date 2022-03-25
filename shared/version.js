021ee22d0e3d4d558c9e56d0653f35f41b312b6a
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-03-25 21:38:58 +0000

    Update version.js

--- shared/version.js
+++ shared/version.js
@@ -1,11 +1,11 @@
-const versionRe = /(?:^R2Northstar\/)(\d+)\.(\d+)\.(\d+)/g
+const versionRe = /(?:^R2Northstar\/)(\d+)\.(\d+)\.(\d+)(\+dev)?/g
 
 function minimumVersion( request )
 {
 	let v = versionRe.exec( request.headers["User-Agent"] )
 	if( !process.env.MINIMUM_LAUNCHER_VERSION || request.headers["User-Agent"].match ( /(\+dev)/ ) ) return true
 	let minV = process.env.MINIMUM_LAUNCHER_VERSION.split( "." )
-	if( v &&  v[1] >= minV[0] && v[2] >= minV[1] && v[3] >= minV[2] ) return true
+	if( v &&  v[1] >= minV[0] && v[2] >= minV[1] && v[3] >= minV[2] || v[4] ) return true
 	return false
 }
 

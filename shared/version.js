627df265d284e8b7b18bb2e9c730caeebd742a49
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-03-25 21:56:52 +0000

    efficienc e

--- shared/version.js
+++ shared/version.js
@@ -5,7 +5,7 @@ function minimumVersion( request )
 	if( !process.env.MINIMUM_LAUNCHER_VERSION ) return true
 	if( !request.headers["user-agent"] || !request.headers["user-agent"].startsWith( "R2Northstar/" ) ) return false
 	let v = request.headers["user-agent"].split( " " )[0].substring( 12 )
-	if( semver.valid( v ) && semver.satisfies( v, ">="+process.env.MINIMUM_LAUNCHER_VERSION ) || v.match( /\+dev/g ) ) return true
+	if( v.match( /\+dev/g ) || semver.valid( v ) && semver.satisfies( v, ">="+process.env.MINIMUM_LAUNCHER_VERSION ) ) return true
 	return false
 }
 

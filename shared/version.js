496d0591c7a7b213fcb4694e9531fd6bd7ba1d6d
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2022-03-26 01:49:30 +0100

    Dev version is always allowed regardless of version requirement
    The idea being that contributors can compile source from source without having to change version number
    By default source code is version `0.0.0+dev`
    
    Co-authored-by: pg9182 <96569817+pg9182@users.noreply.github.com>

--- shared/version.js
+++ shared/version.js
@@ -5,7 +5,7 @@ function minimumVersion( request )
 	if( !process.env.MINIMUM_LAUNCHER_VERSION ) return true
 	if( !request.headers["user-agent"] || !request.headers["user-agent"].startsWith( "R2Northstar/" ) ) return false
 	let v = request.headers["user-agent"].split( " " )[0].substring( 12 )
-	return semver.valid( v ) && semver.gte( v, process.env.MINIMUM_LAUNCHER_VERSION )
+	return v.match( /\+dev/g ) || semver.valid( v ) && semver.gte( v, process.env.MINIMUM_LAUNCHER_VERSION )
 }
 
 module.exports = {

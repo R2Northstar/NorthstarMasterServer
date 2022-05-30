de888e569f696b8ad3311b8381e0ed2315cb9d64
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-05-30 18:46:51 +0100

    Client data
    Mainemenupromos, announcements, server list

---
+++ client/client_data.js
@@ -0,0 +1,95 @@
+const path = require( "path" )
+const fs = require( "fs" )
+
+const { getRatelimit } = require( "../shared/ratelimit.js" )
+const { getLastUpdated, getServerList, updateServerList } = require( "../shared/serverlist.js" )
+
+let promodataPath = path.join( __dirname, "mainmenupromodata.json" )
+
+// watch the mainmenupromodata file so we can update it without a masterserver restart
+// eslint-disable-next-line
+fs.watch( promodataPath, ( curr, prev ) =>
+{
+	try
+	{
+		mainMenuPromoData = JSON.parse( fs.readFileSync( promodataPath ).toString() )
+		console.log( "updated main menu promo data successfully!" )
+	}
+	catch ( ex )
+	{
+		console.log( `encountered error updating main menu promo data: ${ ex }` )
+	}
+
+} )
+
+let mainMenuPromoData = {}
+if ( fs.existsSync( promodataPath ) )
+	mainMenuPromoData = JSON.parse( fs.readFileSync( promodataPath ).toString() )
+
+
+let announcementsPath = path.join( __dirname, "announcements.json" )
+
+// watch the announcements file so we can update it without a masterserver restart
+// eslint-disable-next-line
+fs.watch( announcementsPath, ( curr, prev ) =>
+{
+	try
+	{
+		announcementsData = JSON.parse( fs.readFileSync( announcementsPath ).toString() )
+		console.log( "updated main menu promo data successfully!" )
+	}
+	catch ( ex )
+	{
+		console.log( `encountered error updating main menu promo data: ${ ex }` )
+	}
+
+} )
+
+let announcementsData = {}
+if ( fs.existsSync( announcementsPath ) )
+	announcementsData = JSON.parse( fs.readFileSync( announcementsPath ).toString() )
+
+module.exports = ( fastify, opts, done ) =>
+{
+	// exported routes
+
+	// GET /client/mainmenupromos
+	// returns main menu promo info
+	fastify.get( "/client/mainmenupromos",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__CLIENT_MAINMENUPROMOS" ) }, // ratelimit
+		},
+		async ( ) =>
+		{
+			return mainMenuPromoData
+		} )
+
+	// GET /client/announcements
+	// returns global announcements
+	fastify.get ( "/client/announcements",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__CLIENT_ANNOUNCEMENTS" ) }, // ratelimit
+		},
+		async ( ) =>
+		{
+			return announcementsData
+		} )
+
+	// GET /client/servers
+	// returns a list of available servers
+	fastify.get( "/client/servers",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__CLIENT_SERVERS" ) }, // ratelimit
+		},
+		async ( ) =>
+		{
+			if ( Date.now() > getLastUpdated() + 1000 )
+			{
+				updateServerList()
+			}
+
+			return getServerList()
+		} )
+
+	done()
+}
---
+++ shared/serverlist.js
@@ -0,0 +1,57 @@
+let serverList = []
+let lastUpdated = Date.now()
+
+const { GameServer, GetGameServers, RemoveGameServer } = require( "../shared/gameserver.js" )
+
+module.exports = {
+	getLastUpdated: function ()
+	{
+		return lastUpdated
+	},
+	getServerList: function ()
+	{
+		return serverList
+	},
+	updateServerList: function ()
+	{
+		let displayServerArray = []
+		let expiredServers = []
+		let servers = Object.values( GetGameServers() )
+
+		for ( let i = 0; i < servers.length; i++ )
+		{
+			// prune servers if they've had 30 seconds since last heartbeat
+			if ( Date.now() - servers[ i ].lastHeartbeat > 30000 )
+			{
+				// console.log(`REMOVE: (${servers[i].id}) - ${servers[i].name}`)
+				expiredServers.push( servers[ i ] )
+				continue
+			}
+
+			// don't show non-private_match servers on lobby since they'll pollute server list
+			if ( servers[ i ].map == "mp_lobby" && servers[ i ].playlist != "private_match" )
+			{
+				// console.log(`SKIP: (${servers[i].id}) - ${servers[i].name}`)
+				continue
+			}
+
+			// create a copy of the gameserver obj for clients so we can hide sensitive info
+			let copy = new GameServer( servers[ i ] )
+			delete copy.ip
+			delete copy.port
+			delete copy.authPort
+			delete copy.password
+			delete copy.serverAuthToken
+
+			// console.log(`ALLOW: (${servers[i].id}) - ${servers[i].name}`)
+			displayServerArray.push( copy )
+		}
+
+		// delete servers that we've marked for deletion
+		for ( let server of expiredServers )
+			RemoveGameServer( server )
+
+		serverList = displayServerArray
+		lastUpdated = Date.now()
+	}
+}

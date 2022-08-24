4fa5307c1c3bdbd7605596978ae85ba28934a016
Remy Raes <contact@remyraes.com>
2022-08-24 21:36:46 +0200

    feat: add basic verified_mods resource

---
+++ client/verified_mods.js
@@ -0,0 +1,42 @@
+const path = require( "path" )
+const fs = require( "fs" )
+
+const { getRatelimit } = require( "../shared/ratelimit.js" )
+const verifiedModsPath = path.join( __dirname, "resource", "verified_mods.json" );
+
+// watch the JSON file so we can update it without a masterserver restart
+fs.watch( verifiedModsPath, ( curr, prev ) =>
+{
+	try
+	{
+		verifiedModsList = JSON.parse( fs.readFileSync( verifiedModsPath ).toString() )
+		console.log( "Updated verified mods list successfully!" )
+	}
+	catch ( ex )
+	{
+		console.log( `Encountered error updating verified mods list: ${ ex }` )
+	}
+
+} )
+
+let verifiedModsList = [];
+if ( fs.existsSync( verifiedModsPath ) )
+	verifiedModsList = JSON.parse( fs.readFileSync( verifiedModsPath ).toString() )
+
+module.exports = ( fastify, opts, done ) =>
+{
+	// exported routes
+
+	// GET /client/verifiedmods
+	// returns a list of manually-verified mods
+	fastify.get( "/client/verifiedmods",
+		{
+			config: { rateLimit: getRatelimit( "REQ_PER_MINUTE__CLIENT_MAINMENUPROMOS" ) }, // ratelimit
+		},
+		async ( ) =>
+		{
+			return verifiedModsList
+		} )
+
+	done()
+}

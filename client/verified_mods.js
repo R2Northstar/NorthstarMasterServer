013ae7ed33d4301fd442a7a4ada256a1fed75017
Remy Raes <raes.remy@gmail.com>
2022-08-25 22:58:37 +0200

    fix: remove duplicated entries

--- client/verified_mods.js
+++ client/verified_mods.js
@@ -3,24 +3,39 @@ const fs = require( "fs" )
 
 // Checks if a mod entry respects the mod naming convention (AuthorName-ModName-Version).
 // https://northstar.thunderstore.io/package/create/docs/ (check dependencies format)
-function checkModEntry (modName) {
-	return /^[a-zA-Z0-9\_]+-[a-zA-Z0-9\_]+-[0-9]+\.[0-9]+\.[0-9]+$/.test(modName);
+function checkModEntry ( modName )
+{
+	return /^[a-zA-Z0-9_]+-[a-zA-Z0-9_]+-[0-9]+\.[0-9]+\.[0-9]+$/.test( modName )
 }
 
 // Remove mod entries that do not match naming convention from exposed mods list.
-function checkAllMods() {
-	for (let i=0; i<verifiedModsList.length; i++) {
+function checkAllMods()
+{
+	for ( let i=0; i<verifiedModsList.length; i++ )
+	{
 	// for (const mod of verifiedModsList) {
-		const mod = verifiedModsList[i];
+		const mod = verifiedModsList[i]
 
-		if (!checkModEntry(mod)) {
-			console.warn(`Since "${mod}" does not respect mod naming convention, it was removed from verified mods list.`);
-			let index = verifiedModsList.indexOf(mod);
-			while (index !== -1) {
-				verifiedModsList.splice(index, 1);
-				index = verifiedModsList.indexOf(mod);
-				i -= 1;
+		if ( !checkModEntry( mod ) )
+		{
+			console.warn( `Since "${mod}" does not respect mod naming convention, it was removed from verified mods list.` )
+			let index = verifiedModsList.indexOf( mod )
+			while ( index !== -1 )
+			{
+				verifiedModsList.splice( index, 1 )
+				index = verifiedModsList.indexOf( mod )
+				i -= 1
 			}
+			continue
+		}
+
+		// Remove duplicated entries
+		let entryCount = verifiedModsList.filter( entry => entry === mod ).length
+		while ( entryCount !== 1 )
+		{
+			verifiedModsList.splice( verifiedModsList.indexOf( mod, i+1 ), 1 )
+			entryCount = verifiedModsList.filter( entry => entry === mod ).length
+			console.warn( `"${mod}" entry was found several times, deleting duplicate.` )
 		}
 	}
 }
@@ -44,7 +59,8 @@ fs.watch( verifiedModsPath, () =>
 } )
 
 let verifiedModsList = []
-if ( fs.existsSync( verifiedModsPath ) ) {
+if ( fs.existsSync( verifiedModsPath ) )
+{
 	verifiedModsList = JSON.parse( fs.readFileSync( verifiedModsPath ).toString() )
 	checkAllMods()
 }

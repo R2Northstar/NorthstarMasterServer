baeb11fdb140bbba2078aabd4621966952cc1c23
Tristan Kennedy <tristan@taskinoz.com>
2022-08-28 16:07:23 +1000

    Made blog page dynamic

--- client/landing.js
+++ client/landing.js
@@ -4,10 +4,18 @@ const path = require( "path" )
 const fastifyStatic = require( "fastify-static" )
 const showdown  = require( "showdown" )
 const converter = new showdown.Converter()
+// Blog Templates
+const blogdir = path.join( __dirname, "../web/blog/" )
+const head = fs.readFileSync( blogdir+"blog-head.html", "utf-8" )
+const foot = fs.readFileSync( blogdir+"blog-footer.html", "utf-8" )
+
 
 module.exports = ( fastify, opts, done ) =>
 {
 	// exported routes
+
+	// Preload blog posts
+	const postData = getPosts()
 
 	// Blog
 	fastify.get( "/blog",
@@ -16,7 +24,21 @@ module.exports = ( fastify, opts, done ) =>
 		},
 		async ( request, reply ) =>
 		{
-			const stream = fs.createReadStream( path.join( __dirname, "../web/blog/index.html" ), "utf-8" )
+			let posts = ""
+			postData.map( ( post, index ) => posts+=`
+				<a href="/blog/${post.link}" class="post" style="background-position-y: ${100*index}px">
+					<div class="overlay">
+						<div class="post-content">
+							<div class="link-title">${post.title}</div>
+							<div class="link-content">${post.blurb}</div>
+						</div>
+						<div class="post-read">
+							<div class="link-post">Read Post &rsaquo;</div>
+						</div>
+					</div>
+				</a>
+			` )
+			const stream = `${head}\n${posts}\n${foot}`
 			reply.type( "text/html" ).send( stream )
 		}
 	)
@@ -29,6 +51,13 @@ module.exports = ( fastify, opts, done ) =>
 		async ( request, reply ) =>
 		{
 			const { post } = request.params
+
+			const breadcrumbs = `
+				<div class="breadcrumbs">
+					<span><a href="/blog">/blog</a>/${post}</span>
+				</div>
+			`
+
 			if ( !post )
 			{
 				reply.redirect( "/blog" )
@@ -38,7 +67,7 @@ module.exports = ( fastify, opts, done ) =>
 
 			if ( postData )
 			{
-				reply.type( "text/html" ).send( postData )
+				reply.type( "text/html" ).send( `${head}\n${breadcrumbs}\n${postData}\n${foot}` )
 			}
 			reply.code( 404 ).send( { error: "Not Found", message: `Route GET:/blog/${post} not found`, statusCode: 404 } )
 
@@ -78,10 +107,6 @@ module.exports = ( fastify, opts, done ) =>
 
 const readPost = ( post ) =>
 {
-	const blogdir = path.join( __dirname, "../web/blog/" )
-	const head = fs.readFileSync( blogdir+"blog-head.html", "utf-8" )
-	const foot = fs.readFileSync( blogdir+"blog-footer.html", "utf-8" )
-
 	if ( !fs.existsSync( `${blogdir + post}.md` ) )
 	{
 		return false
@@ -91,6 +116,37 @@ const readPost = ( post ) =>
 	let fileData = fs.readFileSync( `${blogdir+post}.md`, "utf-8" )
 	let html = converter.makeHtml( fileData )
 
-	return `${head}\n${html}\n${foot}`
+	return html
 
+}
+
+const getPosts = () =>
+{
+	let allPosts = []
+	//passsing directoryPath and callback function
+	fs.readdir( blogdir, function ( err, files )
+	{
+		//handling error
+		if ( err )
+		{
+			return console.log( "Unable to scan directory: " + err )
+		}
+
+		//listing all files using forEach
+		files.forEach( function ( file )
+		{
+			if ( file.split( "." )[1]==="md" )
+			{
+				let data = fs.readFileSync( blogdir+file, "utf-8" )
+				let headding = data.match( /(?<=(^#)\s).*/g )
+				let paragraph = data.match( /^[A-Za-z].*(?:\n[A-Za-z].*)*/gm )
+				allPosts.push( {
+					link: file.split( "." )[0],
+					title: headding[0],
+					blurb: paragraph[0],
+				} )
+			}
+		} )
+	} )
+	return allPosts
 }
